1c045fb5c1e744ebd93599b7b2810827e24e52f6
dewansh07 <dewanshwadhwa@gmail.com>
2022-11-28 19:45:55 +0530

    intial commit

--- modules/firebase.js
+++ modules/firebase.js
@@ -1,21 +1,21 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBwuD6u960Ejl63O_nPlre4zviMRSve_p4",
-    authDomain: "codeyourown-347217.firebaseapp.com",
-    projectId: "codeyourown-347217",
-    storageBucket: "codeyourown-347217.appspot.com",
-    messagingSenderId: "982106897044",
-    appId: "1:982106897044:web:133d2316b9ce6518df5281",
-    measurementId: "G-0PE9NMY8PE"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArOPsOzrhJc4WyhJRw8BRVrzTll5iK2sY",
+  authDomain: "code-your-own.firebaseapp.com",
+  projectId: "code-your-own",
+  storageBucket: "code-your-own.appspot.com",
+  messagingSenderId: "910330298169",
+  appId: "1:910330298169:web:69bb2caae9c6378fcb72e8",
+  measurementId: "G-61HSYLYW51"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
